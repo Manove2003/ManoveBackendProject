d285be391c0f824199e6c696bcd0a3372e4c937c
Manove Kumar <kumarmanove2003@gmail.com>
2024-10-12 00:19:00 +0500

    first commit 2

--- controllers/authController.js
+++ controllers/authController.js
@@ -133,7 +133,8 @@ exports.googleCallback = async (req, res) => {
 
     // Redirect to the frontend with the token and current role
     res.redirect(
-      `https://dreamy-crostata-e2074b.netlify.app/welcome?token=${token}&role=${user.role}`
+      "https://dreamy-crostata-e2074b.netlify.app/welcome"
+      // `https://dreamy-crostata-e2074b.netlify.app/welcome?token=${token}&role=${user.role}`
     );
   } catch (error) {
     console.error("Google callback error:", error);
